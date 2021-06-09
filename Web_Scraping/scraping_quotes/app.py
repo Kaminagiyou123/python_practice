9bbb4268ea94f3dbbb4dbdac40fe096e201fed43
Ran You <ran.you8311@gmail.com>
2021-06-08 19:18:46 -0700

    scraping section complete

--- Web_Scraping/scraping_quotes/app.py
+++ Web_Scraping/scraping_quotes/app.py
@@ -1,8 +1,18 @@
-import requests
+from pages.quotes_page import InvalidTagForAuthorError
+from selenium import webdriver
 from pages.quotes_page import QuotesPage
+try:
+ author=input("Enter the author you'd like qutoes from: ")
+ tag=input("Enter your tag: ")
 
-page_content=requests.get("https://quotes.toscrape.com/")
-page=QuotesPage(page_content)
+ chrome=webdriver.Chrome(executable_path="/Users/ranyou/Downloads/chromedriver")
+ chrome.get("https://quotes.toscrape.com/search.aspx")
+ page=QuotesPage(chrome)
 
-for quote in page.quotes:
- print(quote)
+ print(page.search_for_quotes(author,tag))
+ 
+except InvalidTagForAuthorError as e:
+ print(e)
+except Exception as e:
+ print(e)
+ print("An error has occured")
