716a6d8dcae5d02bf0ba01a7044c01f6ee78ac7e
Ran You <ran.you8311@gmail.com>
2021-06-10 20:25:43 -0700

    asynchronous

--- Web_Scraping_2.2/app.py
+++ Web_Scraping_2.2/app.py
@@ -1,5 +1,9 @@
 import requests
 import logging
+import aiohttp
+import asyncio
+import time
+import async_timeout
 
 from pages.all_books_page import AllBookPage
 
@@ -19,10 +23,32 @@ page=AllBookPage(page_cotent)
 books=page.book;
 max_num=page.page_count
 
-for page_num in range(1,max_num):
- url=f'https://books.toscrape.com/catalogue/page-{page_num}.html'
- page_cotent=requests.get(url).content;
- page=AllBookPage(page_cotent)
+async def fetch_page(session,url):
+ page_start=time.time()
+ async with async_timeout.timeout(10):
+  async with session.get(url) as response:
+    print(f'page took {time.time()-page_start}')
+    return await response.text()
+
+async def get_multiple_pages(loop,*urls) :
+ tasks=[]
+ async with aiohttp.ClientSession(loop=loop) as session:
+  for url in urls:
+   tasks.append(fetch_page(session,url))
+  grouped_tasks=asyncio.gather(*tasks)
+  return await grouped_tasks
+ 
+loop= asyncio.get_event_loop() 
+
+urls=[f'https://books.toscrape.com/catalogue/page-{page_num}.html' for page_num in range(1,page.page_count)]
+start=time.time()
+pages=loop.run_until_complete(get_multiple_pages(loop,*urls))
+print(f"Total page requests took{time.time()-start}")
+
+for page_content in pages:
+ page=AllBookPage(page_content)
  books.extend(page.book)
+
+
  
  
