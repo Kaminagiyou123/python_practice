2effc6029c133ce435a47b89ee2ba66bb4355062
Ran You <ran.you8311@gmail.com>
2021-06-15 20:47:22 -0700

    binary tree complete

--- binary_tree/app.py
+++ binary_tree/app.py
@@ -1,11 +1,11 @@
 from node import Node
 from binary_tree import BinaryTree
 
-tree=BinaryTree(Node(9))
-tree.add(Node(5))
-tree.add(Node(11))
+tree=BinaryTree(Node(6))
+nodes=[5,3,9,7,8,7.5,12,11]
 
-print(tree.head)
-print(tree.head.left)
-print(tree.head)
-print(tree.head.right)
+for n in nodes:
+ tree.add(Node(n))
+ 
+tree.delete(9)
+tree.inorder()
